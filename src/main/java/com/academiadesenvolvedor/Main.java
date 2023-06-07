23782ebdc7260d011176f18054e14cc7913a3149
Nicolly Reis <nicollyro2004@gmail.com>
2023-06-06 21:45:42 -0300

    Implementa cadastro, edição e listagem de produtos e clientes

---
+++ src/main/java/com/academiadesenvolvedor/Main.java
@@ -0,0 +1,17 @@
+package com.academiadesenvolvedor;
+
+import com.academiadesenvolvedor.aplication.App;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner =  new Scanner(System.in);
+        App app = new App (scanner);
+
+        app.executar();
+
+        scanner.close();
+    }
+}
